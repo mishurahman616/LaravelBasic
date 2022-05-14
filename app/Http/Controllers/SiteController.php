cd9039ba8c092104f1f095f45df6b4febee44d0a
Mishu Rahman <mishu.cse616@gmail.com>
2022-05-15 04:16:14 +0600

    Route and Controller is configured

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    function showHome(){
+        return view('home');
+    }
+
+    function showAbout(){
+        return view('about');
+    }
+    function showService(){
+        return view('services');
+    }
+    function showPortfolio(){
+        return view('portfolio');
+
+    }
+}

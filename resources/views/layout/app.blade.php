61a1d45704ba06a70de5d4c27d804a60b0d305fe
Mishu Rahman <mishu.cse616@gmail.com>
2022-05-15 20:52:15 +0600

    CSS and js file moved to public folder and  add in app.blade.php file
    CSS and JS file moved to public folder for ease of accessing them with asset(). and added them to master layout (app.blade.php) file

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+    </head>
+    <body>
+        
+        <script src="{{asset('js/popper.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('js/custom.js')}}"></script>
+    </body>
+    <footer>
+
+    </footer>
+</html>
